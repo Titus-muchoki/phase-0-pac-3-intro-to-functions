c22f480da33f70663e8a76eeecf80cd0ed912a0b
kajela <titoyut56@gmail.com>
2023-03-03 11:10:08 -0500

    add the initial files

--- index.js
+++ index.js
@@ -2,18 +2,32 @@
 function myWoman(){
     console.log("I would be so happy to be on top of gathoni right now");
 }
-myWoman();
+
 function myUniversity(){
     console.log("KCA University");
 }
-myUniversity();
+
 
 function myLife(){
     console.log("Since i focused on me my life have been very smooth");
 }
-myLife()
+
 
 function myHome(){
     console.log("I am from mundoro");
 }
+function doSomething(thing) {
+    console.log(thing);
+  }
+  
+  doSomething("anything");
 myHome();
+myLife();
+myUniversity();
+myWoman();
+
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+  }
+sayHelloTo("kajela");
+sayHelloTo("my family");
