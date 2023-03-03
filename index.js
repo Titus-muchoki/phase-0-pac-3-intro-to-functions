4008ec5f4c9e8a824240ca773f381ff2c7d23567
kajela <titoyut56@gmail.com>
2023-03-03 10:25:24 -0500

    add the initial files

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Follow along with the examples here
+function myWoman(){
+    console.log("I would be so happy to be on top of gathoni right now");
+}
+myWoman();
+function myUniversity(){
+    console.log("KCA University");
+}
+myUniversity();
+
+function myLife(){
+    console.log("Since i focused on me my life have been very smooth");
+}
+myLife()
+
+function myHome(){
+    console.log("I am from mundoro");
+}
+myHome();
